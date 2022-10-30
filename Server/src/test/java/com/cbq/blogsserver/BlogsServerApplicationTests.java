23c7d6861019a201e39c8a3c2cfe89b69d0b7309
Changbaiqi <2084069833@qq.com>
2022-10-30 16:28:16 +0800

    初始化服务端

---
+++ Server/src/test/java/com/cbq/blogsserver/BlogsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbq.blogsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
