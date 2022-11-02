0f164588f45f63dcadc3f8ab3002d3ed7b11e14e
Changbaiqi <2084069833@qq.com>
2022-11-02 16:08:14 +0800

    main页面的顶部标题栏优化

--- WebSite/home/src/components/HeadNavigation.vue
+++ WebSite/home/src/components/HeadNavigation.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="head_navigation">
     <a>长白崎の博客</a>
-        <div><img><span>搜索</span></div>
-        <div><img><span>留言板</span></div>
-        <div><img><span>首页</span></div>
+        <div id="else"><img><span>其他</span>
+            <ul>
+                <li style="text-align: center;">网址收藏</li>
+                <li style="text-align: center;">选项二</li>
+                <li style="text-align: center;">选项三</li>
+            </ul>
+        </div>
+        <div id="board"><img><span>留言板</span></div>
+        <div id="home"><img><span>首页</span></div>
+        <div id="search"><img><span>搜索</span></div>
   </div>
 </template>
 
@@ -17,6 +24,7 @@ export default {
 /* ---------头部导航栏------------ */
 .head_navigation{
     height: 50px;
+    width: 100%;
     z-index: -1;
     position: absolute;
     z-index: 2;
@@ -29,7 +37,7 @@ export default {
     position: relative;
     top: 10px;
     left: 50px;
-    font-size: 28px;
+    font-size: 23px;
 }
 .head_navigation div{
     display: inline;
@@ -38,5 +46,210 @@ export default {
     margin-top: 10px;
     color: rgba(21, 21, 21, 0.748);
     font-size: 16px;
+}
+
+/* 搜索 */
+.head_navigation #search{
+    width: 60px;
+    float: right;
+    position: relative;
+    top: 10px;
+    right: 10px;
+    cursor: url('../assets/images/favicon.ico'),default;
+}
+.head_navigation #search::before{
+    content: "";
+    position: absolute;
+    top: 20px;
+    left: 50%;
+    transform: translate(-50%);
+    border-bottom:5px solid #9a7bd4;
+    width: 0;
+    height: 5px;
+    transition: 0.2s all linear;
+}
+.head_navigation #search:hover::before{
+    width: 100%;
+    transition: 0.2s all linear;
+}
+
+
+.head_navigation #search img{
+    content: url(../assets/images/搜索.png);
+    
+    filter: drop-shadow(0px 0 0 rgb(255, 255, 255));
+    width: 16px;
+    height: 16px;
+}
+.head_navigation #search span{
+    color: #c6c5cd;
+    margin-left: 10px;
+    position: absolute;
+}
+
+/* 首页 */
+.head_navigation #home{
+    width: 60px;
+    float: right;
+    position: relative;
+    top: 10px;
+    cursor: url('../assets/images/favicon.ico'),default;
+}
+.head_navigation #home::before{
+    content: "";
+    position: absolute;
+    top: 20px;
+    left: 50%;
+    transform: translate(-50%);
+    border-bottom:5px solid #9a7bd4;
+    width: 0;
+    height: 5px;
+    transition: 0.2s all linear;
+}
+.head_navigation #home:hover::before{
+    width: 100%;
+    transition: 0.2s all linear;
+}
+
+
+.head_navigation #home img{
+    content: url(../assets/images/首页.png);
+    
+    filter: drop-shadow(0px 0 0 rgb(255, 255, 255));
+    width: 16px;
+    height: 16px;
+}
+.head_navigation #home span{
+    color: #c6c5cd;
+    margin-left: 10px;
+    position: absolute;
+}
+/* 留言板 */
+.head_navigation #board{
+    width: 80px;
+    float: right;
+    position: relative;
+    top: 10px;
+    cursor: url('../assets/images/favicon.ico'),default;
+}
+.head_navigation #board::before{
+    content: "";
+    position: absolute;
+    top: 20px;
+    left: 50%;
+    transform: translate(-50%);
+    border-bottom:5px solid #9a7bd4;
+    width: 0;
+    height: 5px;
+    transition: 0.2s all linear;
+}
+.head_navigation #board:hover::before{
+    width: 100%;
+    transition: 0.2s all linear;
+}
+
+
+.head_navigation #board img{
+    content: url(../assets/images/首页.png);
+    
+    filter: drop-shadow(0px 0 0 rgb(255, 255, 255));
+    width: 16px;
+    height: 16px;
+}
+.head_navigation #board span{
+    color: #c6c5cd;
+    margin-left: 10px;
+    position: absolute;
+}
+
+/* 其他 */
+.head_navigation #else{
+    width: 80px;
+    float: right;
+    position: relative;
+    top: 10px;
+    cursor: url('../assets/images/favicon.ico'),default;
+}
+.head_navigation #else::before{
+    content: "";
+    position: absolute;
+    top: 20px;
+    left: 50%;
+    transform: translate(-50%);
+    border-bottom:5px solid #9a7bd4;
+    width: 0;
+    height: 5px;
+    transition: 0.2s all linear;
+}
+.head_navigation #else:hover::before{
+    width: 100%;
+    transition: 0.2s all linear;
+}
+
+
+.head_navigation #else img{
+    content: url(../assets/images/首页.png);
+    
+    filter: drop-shadow(0px 0 0 rgb(255, 255, 255));
+    width: 16px;
+    height: 16px;
+}
+.head_navigation #else span{
+    color: #c6c5cd;
+    margin-left: 10px;
+    position: absolute;
+}
+.head_navigation #else span::after{
+    /* height: 20px;
+    width: 20px; */
+    content: '';
+    position: absolute;
+    top: 5px;
+    left: 35px;
+    height: 20px;
+    width: 20px;
+    background: url(../assets/images/向下\ \(1\).png) no-repeat;
+    background-size:15px;
+    transition-duration: .5s;
+}
+.head_navigation #else:hover span::after{
+    transform: rotateZ(-180deg);
+    transition-duration: .5s;
+    transform-origin: 40% 35%;
+}
+.head_navigation #else ul{
+    height: 150px;
+    width: 100px;
+    background-color: #000000;
+    position: absolute;
+    top: 50px;
+    right: 5px;
+    box-shadow: 0px 0px 10px 0px #1c1c1c;
+    visibility: hidden;
+    filter: opacity(0);
+    transition-duration: .3s;
+}
+.head_navigation #else:hover ul{
+    visibility: visible;
+    filter: opacity(1);
+    position: absolute;
+    top: 40px;
+    right: 5px;
+    transition-duration: .3s;
+    list-style-type: none;
+}
+.head_navigation #else ul li{
+    color: #c4c4c4;
+    width: 140%;
+    height: calc(33%);
+    line-height: 50px;
+    justify-items: center;
+    /* background-color: #712929; */
+    list-style-type: none;
+    position: relative;
+    left: -40px;
+}
+.head_navigation #else ul li:hover{
+    background-color: #a7a7a744;
 }
 </style>
