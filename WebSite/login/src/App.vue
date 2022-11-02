6afcee661e152373eb364b0c078411c305eb20f5
Changbaiqi <2084069833@qq.com>
2022-11-03 00:46:10 +0800

    更新登录界面的设计

--- WebSite/login/src/App.vue
+++ WebSite/login/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Login id="login"></Login>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
@@ -24,5 +23,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  background-image: url(./assets/images/40d6429d1e82fae5f67787fb896781a0.jpeg);
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+#login{
+  margin: 0 auto;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
 }
 </style>
---
+++ WebSite/login/src/components/Login.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div id="login_main">
+        <div id="title">Login</div>
+        <div class="input">
+            <input id="account" type="text">
+            <input id="password" type="password">
+        </div>
+        <button id="submit">登录</button>
+    </div>
+  </div>
+</template>
+
+<script scoped>
+export default {
+    name: 'Login'
+}
+</script>
+
+<style>
+#login_main{
+    background-color: rgb(41, 41, 41);
+    height: 500px;
+    width: 550px;
+    border-radius: 15px;
+    box-shadow: 0 0 5px 0 rgb(29, 29, 29);
+    animation: loading 1s;
+}
+
+#title{
+    transform: scale(1);
+    transition-duration: 1s;
+}
+#title:hover{
+    transform:scale(1.2);
+    transition-duration: 1s;
+}
+
+@keyframes loading {
+    0%{
+        transform: scale(0);
+    }
+    100%{
+        transform: scale(1);
+    }
+}
+
+#title{
+    color: aliceblue;
+    font-size: 60px;
+    position: relative;
+    top: 10%;
+}
+.input{
+    position: relative;
+    top: 20%;
+    left: 47%;
+    transform: translate(-28%);
+}
+.input #account{
+    height: 45px;
+    width: 300px;
+    display: block;
+    border-radius: 60px;
+    border-style: none;
+    outline: none;
+    padding-left: 20px;
+    padding-right: 20px;
+}
+.input #password{
+    height: 45px;
+    width: 300px;
+    display: block;
+    margin-top: 20px;
+    border-radius: 60px;
+    border: 0;
+    border-style: none;
+    outline: none;
+    padding-left: 20px;
+    padding-right: 20px;
+}
+#submit{
+    position: relative;
+    top: 30%;
+    border-radius: 13px;
+    height:50px;
+    width: 250px;
+    background-color: rgb(108, 108, 108);
+    border: none;
+    transition-duration: .3s;
+    color: aliceblue;
+    font-size: 17px;
+}
+
+#submit:hover{
+    background-color: rgb(77, 77, 77);
+    transition-duration: .3s;
+    color: rgb(207, 207, 207);
+}
+</style>
