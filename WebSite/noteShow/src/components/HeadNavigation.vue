a38dea68edf3661cb90c415f43302ec64d1371c3
Changbaiqi <2084069833@qq.com>
2022-12-04 15:22:41 +0800

    更新msgboard代码

--- WebSite/noteShow/src/components/HeadNavigation.vue
+++ WebSite/noteShow/src/components/HeadNavigation.vue
@@ -8,6 +8,7 @@
                 <li style="text-align: center;">选项三</li>
             </ul>
         </div>
+        <a id="aboutme"><img><span>关于</span></a>
         <a id="board"><img><span>留言板</span></a>
         <a id="home"><img><span>首页</span></a>
         <a id="search"><img><span>搜索</span></a>
@@ -20,11 +21,12 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 /* ---------头部导航栏------------ */
 .head_navigation{
     height: 50px;
     width: 100%;
+    min-width: 1150px;
     z-index: -1;
     position: absolute;
     z-index: 2;
@@ -136,7 +138,7 @@ export default {
     float: right;
     position: relative;
     top: 10px;
-    margin-right: 100px;
+    margin-right: 50px;
     cursor: url('../assets/images/favicon.ico'),default;
 }
 .head_navigation #board::before{
@@ -157,11 +159,11 @@ export default {
 
 
 .head_navigation #board img{
-    content: url(../assets/images/首页.png);
+    content: url(../assets/images/留言.svg);
     
     filter: drop-shadow(0px 0 0 rgb(255, 255, 255));
-    width: 16px;
-    height: 16px;
+    width: 18px;
+    height: 18px;
 }
 .head_navigation #board span{
     color: #c6c5cd;
@@ -171,13 +173,54 @@ export default {
     font-size: 17px;
 }
 
+/* 关于 */
+.head_navigation #aboutme{
+    width: 65px;
+    height: 70px;
+    float: right;
+    position: relative;
+    top: 10px;
+    margin-right: 100px;
+    cursor: url('../assets/images/favicon.ico'),default;
+}
+
+.head_navigation #aboutme::before{
+    content: "";
+    position: absolute;
+    top: 30px;
+    left: 50%;
+    transform: translate(-50%);
+    border-bottom:3px solid #9a7bd4;
+    width: 0;
+    height: 5px;
+    transition: 0.2s all linear;
+}
+.head_navigation #aboutme:hover::before{
+    width: 100%;
+    transition: 0.2s all linear;
+}
+
+.head_navigation #aboutme img{
+    content: url(../assets/images/喜爱.svg);
+    
+    filter: drop-shadow(0px 0 0 rgb(255, 255, 255));
+    width: 17px;
+    height: 17px;
+}
+.head_navigation #aboutme span{
+    color: #c6c5cd;
+    margin-left: 10px;
+    position: absolute;
+    top: 5px;
+    font-size: 17px;
+}
 
 /* 其他 */
 .head_navigation #else{
     width: 80px;
     float: right;
     position: relative;
-    top: 10px;
+    top: 8px;
     cursor: url('../assets/images/favicon.ico'),default;
 }
 .head_navigation #else::before{
@@ -198,7 +241,7 @@ export default {
 
 
 .head_navigation #else img{
-    content: url(../assets/images/首页.png);
+    content: url(../assets/images/杯子.svg);
     
     filter: drop-shadow(0px 0 0 rgb(255, 255, 255));
     width: 16px;
@@ -229,12 +272,13 @@ export default {
     transition-duration: .5s;
     transform-origin: 40% 35%;
 }
+
 .head_navigation #else ul{
     height: 150px;
     width: 130px;
     background-color: #000000;
     position: absolute;
-    top: 40px;
+    top: 50px;
     right: 5px;
     overflow: hidden;
     
@@ -246,6 +290,7 @@ export default {
     display: inline-block;
     letter-spacing: normal;
 }
+
 .head_navigation #else:hover ul{
     visibility: visible;
     filter: opacity(1);
@@ -255,6 +300,7 @@ export default {
     transition-duration: .3s;
     list-style-type: none;
 }
+
 .head_navigation #else ul li{
     color: #c4c4c4;
     width: 130px;
@@ -262,7 +308,7 @@ export default {
     /* background-color: #712929; */
     list-style-type: none;
     position: relative;
-    left: 0px;
+    /* left: -40px; */
 }
 .head_navigation #else ul li:hover{
     background-color: #a7a7a744;
